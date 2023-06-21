ec1cb90634f4065262dc8cf6b0156fbce583ac48
Maria Camila Rodriguez Guerrero <ing.camilarodriguez@gmail.com>
2023-06-21 13:17:52 -0500

    Validations Form workers

--- client/src/componentsSuperAdmin/FormWorkersRegistration/FormWorkersRegistration.jsx
+++ client/src/componentsSuperAdmin/FormWorkersRegistration/FormWorkersRegistration.jsx
@@ -1,16 +1,12 @@
-import style from "./FormWorkersRegistration.module.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { postWorker,
-   getWorkers,
-} from "../../redux/actions/actions";
+import { postWorker, getWorkers } from "../../redux/actions/actions";
 
+import style from "./FormWorkersRegistration.module.css";
 
 const FormWorkers = () => {
-  const { workersData } = useSelector(
-    (state) => state
-  );
+  const { workersData } = useSelector((state) => state);
   const [form, setForm] = useState({
     rut_passport: "",
     address: "",
@@ -33,41 +29,70 @@ const FormWorkers = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     location.state === null && navigate("/su/workersregistration");
   }, [location.state, navigate]);
 
-
-
   const handleSubmit = (e) => {
-    e.preventDefault();    
-    dispatch(postWorker(form));
+    e.preventDefault();
+
+    // Validar campos obligatorios
+    let hasErrors = false;
+    const newErrors = {
+      rut_passport: "",
+      address: "",
+      name: "",
+      profitPercentage: "",
+      goal: "",
+      branch: "",
+      percentageAfterGoal: ""
+    };
+
+    if (form.rut_passport.trim() === "") {
+      newErrors.rut_passport = "El RUT o pasaporte es obligatorio";
+      hasErrors = true;
+    }
+
+    if (form.name.trim() === "") {
+      newErrors.name = "El nombre es obligatorio";
+      hasErrors = true;
+    }
+
+    if (form.branch.trim() === "") {
+      newErrors.branch = "La sede es obligatoria";
+      hasErrors = true;
+    }
+
+    if (form.profitPercentage.trim() === "") {
+      newErrors.profitPercentage = "El porcentaje de ganancias es obligatorio";
+      hasErrors = true;
+    }
+
+    if (form.goal.trim() === "") {
+      newErrors.goal = "La meta es obligatoria";
+      hasErrors = true;
+    }
+
+    if (form.percentageAfterGoal.trim() === "") {
+      newErrors.percentageAfterGoal = "El porcentaje después de la meta es obligatorio";
+      hasErrors = true;
+    }
+
+    setErrors(newErrors);
+
+    if (!hasErrors) {
+      dispatch(postWorker(form));
+    }
   };
 
-  const changehandler = (event) => {
-    const property = event.target.name;
-    const value = event.target.value;
-    setForm({
-      ...form,
-      [property]: value,
-    });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
   };
 
-  const selectStyles = {
-    indicatorSeparator: (styles) => ({ ...styles, display: "none" }),
-    valueContainer: (styles) => ({ ...styles, paddingLeft: "36px" }),
-  };
-
-  const selectTheme = (theme) => ({
-    ...theme,
-    borderRadius: "20px",
-    colors: {
-      ...theme.colors,
-      primary25: "#3cd8f0",
-      primary: "black",
-    },
-  });
   return (
     <div>
       <Link to={"/createVehicle"}>
@@ -79,8 +104,7 @@ const FormWorkers = () => {
       </Link>
       <h1 className={style.title}>Registro de trabajadores</h1>
       <p className={style.subtitle}>Los campos con * son obligatorios</p>
-      <form onSubmit={handleSubmit}>       
-        
+      <form onSubmit={handleSubmit}>
         <div className={style.form}>
           <div className={style.column}>
             <label className={style.label}>RUT ó Pasaporte*</label>
@@ -89,63 +113,69 @@ const FormWorkers = () => {
               type="text"
               value={form.rut_passport}
               className={style.input}
-              onChange={changehandler}
+              onChange={handleChange}
             />
             {errors.rut_passport && <p className={style.error}>{errors.rut_passport}</p>}
+            
             <label className={style.label}>Nombre*</label>
             <input
               name="name"
               type="text"
               value={form.name}
               className={style.input}
-              onChange={changehandler}
+              onChange={handleChange}
             />
             {errors.name && <p className={style.error}>{errors.name}</p>}
+            
             <label className={style.label}>Dirección</label>
             <input
               name="address"
               type="text"
               value={form.address}
               className={style.input}
-              onChange={changehandler}
-            />  
-            <label className={style.label}>Sede</label>
+              onChange={handleChange}
+            />
+            
+            <label className={style.label}>Sede*</label>
             <input
               name="branch"
               type="text"
               value={form.branch}
               className={style.input}
-              onChange={changehandler}
-            />           
-                        
+              onChange={handleChange}
+            />
+            {errors.branch && <p className={style.error}>{errors.branch}</p>}
           </div>
           <div className={style.column}>
-            <label className={style.label}>Porcentaje de ganancias</label>
+            <label className={style.label}>Porcentaje de ganancias*</label>
             <input
               name="profitPercentage"
               type="number"
               value={form.profitPercentage}
               className={style.input}
-              onChange={changehandler}
+              onChange={handleChange}
             />
-            <label className={style.label}>Meta</label>
+            {errors.profitPercentage && (
+              <p className={style.error}>{errors.profitPercentage}</p>
+            )}
+            
+            <label className={style.label}>Meta*</label>
             <input
               name="goal"
               type="number"
               value={form.goal}
               className={style.input}
-              onChange={changehandler}
+              onChange={handleChange}
             />
-            {errors.goal && (
-              <p className={style.error}>{errors.goal}</p>
-            )}
-            <label className={style.label}>Porcentaje despues de la meta</label>
+            {errors.goal && <p className={style.error}>{errors.goal}</p>}
+            
+            <label className={style.label}>Porcentaje despues de la meta*</label>
             <input
               name="percentageAfterGoal"
               type="number"
               value={form.percentageAfterGoal}
               className={style.input}
-              onChange={changehandler}
+              onChange={handleChange}
             />
             {errors.percentageAfterGoal && (
               <p className={style.error}>{errors.percentageAfterGoal}</p>
